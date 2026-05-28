010e95804e6e8b2f3bae6dd369a0c737e4e0d161
Danendra Indiarto <indiartodanendra@gmail.com>
2026-05-29 01:02:08 +0700

    ready

--- app/admin/cashier/page.tsx
+++ app/admin/cashier/page.tsx
@@ -34,7 +34,7 @@ export default function ManageCashierPage() {
     }
 
     try {
-      const res = await fetch(`${API_URL}/auth/register`, {
+      const res = await fetch(`${API_URL}/auth/create-cashier`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
